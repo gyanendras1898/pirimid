e59a325f286edb15aad765788bc6b6090db1332c
gyanendra <gyanendras1898@gmail.com>
2021-08-21 01:19:13 +0530

    added exceptions when wrong input

--- fruit_trader.java
+++ fruit_trader.java
@@ -23,6 +23,7 @@ public class fruit_trader {
                 String input = br.readLine();
                 if (input.equals("PROFIT"))
                     System.out.println(totalProfit);
+
                 else {
                     String[] inputArr = input.trim().split(" ");
                     String opperation = inputArr[0];
@@ -32,17 +33,19 @@ public class fruit_trader {
                     Stock stock = new Stock(priceRate, quantity);
 
                     if (opperation.equals("BUY")) {
-                        System.out.println("BOUGHT " + quantity + " KG APPLE AT " + priceRate + " RUPEES/KG");
+                        System.out
+                                .println("BOUGHT " + quantity + " KG " + fruitName + " AT " + priceRate + " RUPEES/KG");
                         buyFruit(fruitName, stock);
                     }
 
                     else {
-                        System.out.println("SOLD " + quantity + " KG APPLE AT " + priceRate + " RUPEES/KG");
+                        System.out.println("SOLD " + quantity + " KG " + fruitName + " AT " + priceRate + " RUPEES/KG");
                         sellFruit(fruitName, stock);
                     }
 
                 }
             } catch (Exception e) {
+                System.out.println(e);
                 break;
             }
         }
@@ -58,27 +61,37 @@ public class fruit_trader {
         }
     }
 
-    private static void sellFruit(String fruitName, Stock stock) {
+    private static void sellFruit(String fruitName, Stock stock) throws Exception {
+        if (!fruit_cart.containsKey(fruitName)) {
+            throw new Exception("Error : You haven't bought " + fruitName);
+        }
         int availableQuantity = stock.quantity;
         int SP = availableQuantity * stock.priceRate;
         int CP = 0;
         Deque<Stock> deque = fruit_cart.get(fruitName);
 
         while (availableQuantity != 0) {
-            Stock front = deque.poll();
-            if (front.quantity <= availableQuantity) {
-                int quantitySold = front.quantity;
-                CP += quantitySold * front.priceRate;
-                availableQuantity -= quantitySold;
+            if (!deque.isEmpty()) {
+                Stock front = deque.poll();
+                if (front.quantity <= availableQuantity) {
+                    int quantitySold = front.quantity;
+                    CP += quantitySold * front.priceRate;
+                    availableQuantity -= quantitySold;
+                } else {
+                    int quantitySold = availableQuantity;
+                    CP += quantitySold * front.priceRate;
+                    int remainingQuantity = front.quantity - quantitySold;
+                    front.quantity = remainingQuantity;
+                    deque.addFirst(front);
+                    availableQuantity = 0;
+                }
             } else {
-                int quantitySold = availableQuantity;
-                CP += quantitySold * front.priceRate;
-                int remainingQuantity = front.quantity - quantitySold;
-                front.quantity = remainingQuantity;
-                deque.addFirst(front);
-                availableQuantity = 0;
+                fruit_cart.remove(fruitName);
+                throw new Exception("Error : there is no " + fruitName + " left to SELL");
             }
         }
+        if (deque.isEmpty())
+            fruit_cart.remove(fruitName);
         totalProfit += (SP - CP);
     }
 }
